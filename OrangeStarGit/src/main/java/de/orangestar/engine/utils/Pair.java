7ccf207def57d24680022875c49462bb34d06a69
n4n0lix <basti.funck@gmail.com>
2015-05-31 22:50:23 +0200

    Added generic pair class

---
+++ OrangeStarGit/src/main/java/de/orangestar/engine/utils/Pair.java
@@ -0,0 +1,13 @@
+package de.orangestar.engine.utils;
+
+public class Pair<K, V> {
+
+    public Pair(K k, V v) {
+        x = k;
+        y = v;
+    }
+    
+    public K x;
+    public V y;
+    
+}
