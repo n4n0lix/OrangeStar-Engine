6b673c1f75f8671fb1cda515484319f905420c9e
3bartels <3bartels@rzpc220.informatik.uni-hamburg.de>
2015-06-08 16:49:42 +0200

    addedd basic implementation for PlayableGroup

--- OrangeStarGit/src/main/java/de/orangestar/game/game_objects/PlayableGroup.java
+++ OrangeStarGit/src/main/java/de/orangestar/game/game_objects/PlayableGroup.java
@@ -1,7 +1,24 @@
 package de.orangestar.game.game_objects;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.orangestar.engine.logic.GameObject;
 
 public class PlayableGroup extends GameObject {
-
+	
+	private List<Player> _playerList;
+	
+	public PlayableGroup(Player player) {
+		_playerList = new ArrayList<>();
+		_playerList.add(player);
+	}
+	
+	public void addPlayer(Player player) {
+		_playerList.add(player);
+	}
+	
+	public void removePlayer(Player player) {
+		_playerList.remove(player);
+	}
 }
