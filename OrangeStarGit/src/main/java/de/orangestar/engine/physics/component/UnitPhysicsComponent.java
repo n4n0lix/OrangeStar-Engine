a169b5f2a40b6143029b6fc66219758743f197b6
n4n0lix <basti.funck@gmail.com>
2015-06-14 20:12:01 +0200

    Added delta time calculation into the physics module, so that a per second value may now be set at the UnitPhysicsComponent

--- OrangeStarGit/src/main/java/de/orangestar/engine/physics/component/UnitPhysicsComponent.java
+++ OrangeStarGit/src/main/java/de/orangestar/engine/physics/component/UnitPhysicsComponent.java
@@ -1,6 +1,7 @@
 package de.orangestar.engine.physics.component;
 
 import de.orangestar.engine.GameObject;
+import de.orangestar.engine.logic.GameManager;
 import de.orangestar.engine.values.Vector3f;
 
 public class UnitPhysicsComponent extends PhysicsComponent {
@@ -16,9 +17,9 @@ public class UnitPhysicsComponent extends PhysicsComponent {
 	@Override
 	public void onUpdate() {
 		Vector3f position = getGameObject().getLocalTransform().position;
-		position.x += _velocityX;
-		position.y += _velocityY;
-		position.z += _velocityZ;
+		position.x += _velocityX * GameManager.DELTA_TIME;
+		position.y += _velocityY * GameManager.DELTA_TIME;
+		position.z += _velocityZ * GameManager.DELTA_TIME;
 		
 		_velocityX = 0;
 		_velocityY = 0;
